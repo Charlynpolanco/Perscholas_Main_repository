06796d2564e729cad4e315be87f2b656916c4dee
Charlynpolanco <charlynpolanco@gmail.com>
2022-08-09 23:34:41 -0400

    homework 1 exercises

---
+++ Dev/Homework/Homework 1.js
@@ -0,0 +1,31 @@
+//First exercise
+let num1=2
+let num2=1
+let num3=num1+num2
+
+console.log(num3)
+//Second exercise
+let num4=4
+let num5=8
+let num6=(num3-num2)+(num5/num4)
+console.log(num6)
+//Third Exercise
+let num7=5
+let num8=9
+let=num9=1
+let num10=7
+
+//average formula
+let average1=(num1+num2+num3+num4+num5)/(5)
+let average2=(num6+num7+num8+num9+num10)/(5)
+console.log("Here is the average of the first 5 numbers", average1)
+console.log("Here is the average of the last 5 numbers", average2)
+
+//Fourth exercise
+//Find the average of 4, 10, 22, -30, 55 and 68, 7, -22, 9, 100
+
+average1=(4+10+22+-30+55)/(5)
+average2=(68+7+-22+9+100)/(5)
+average3=(average1+average2)/(2)
+console.log("the first 5 numbers average is ", average1, ". The average of the 5 last numbers is ", average2)
+console.log("The average of the averages is", average3.toFixed(2))
